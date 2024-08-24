f096f1d3b563efb7abd74b3342e74795600e7c46
Anne Hunt <acathunt@gmail.com>
2024-08-24 15:45:46 -0600

    fizzbuzz complete

--- programstructure.js
+++ programstructure.js
@@ -1,5 +1,25 @@
+//NOTE - FOR LOOP
+
 let x = ''
 for (let i = 0; i < 7; i++) {
     x += '#'
     console.log(x)
 }
+
+//NOTE - FIZZBUZZ
+
+for (let i = 1; i < 101; i++) {
+    if (i % 3 < 1 && i % 5 < 1) {
+        console.log('fizzbuzz')
+    }
+    if (i % 3 < 1) {
+        console.log('fizz')
+    }
+    else if (i % 5 < 1) {
+        console.log('buzz')
+    }
+    else
+        console.log(i)
+}
+
+//NOTE - Chessboard

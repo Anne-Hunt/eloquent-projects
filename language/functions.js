2bf12f5f2437b349e3ec06cde8f85dad7637f70f
Anne Hunt <acathunt@gmail.com>
2024-08-28 12:03:27 -0600

    finished chapter

--- language/functions.js
+++ language/functions.js
@@ -21,3 +21,25 @@ function isEven(x) {
         return true
     }
 }
+
+// Bean Counting
+
+function countBs(x) {
+    let count = 0
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] === 'B') {
+            count++
+        }
+    }
+    return count
+}
+
+function countChar(x, y) {
+    let count = 0;
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] === y) {
+            count++
+        }
+    }
+    return count;
+}

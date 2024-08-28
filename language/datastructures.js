9f7a428c863828509acd3776ef8171e6445f181c
Anne Hunt <acathunt@gmail.com>
2024-08-28 13:45:13 -0600

    finished first challenge

--- language/datastructures.js
+++ language/datastructures.js
@@ -1,16 +1,27 @@
 // The Sum of a Range
 let z = []
 function range(x, y) {
+    z = []
     for (let i = x; i <= y; i++) {
         z.push(i)
     }
     return z
 }
 
-function sum(z) {
+function sum(a) {
     let total = 0
-    for (let i = 0; i < z.length; i++) {
-        total + i
+    for (let i = 0; i <= a.length; i++) {
+        total = total + i
     }
     return total
+}
+
+// Reversing an Array
+
+function reverseArray(x) {
+
+}
+
+function reverseArrayInPlace(x) {
+
 }
